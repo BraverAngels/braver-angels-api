8e10929dd6867d7d54088cdb31b012982c3c1f42
Blake Lundquist <lundqub@outlook.com>
2020-04-30 12:18:40 -0700

    post subscriber action network submission

--- src/subscribe.js
+++ src/subscribe.js
@@ -23,24 +23,43 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.post('/', urlencodedParser, (req, res) => {
   req.log.info(req.body)
 
-  res.status(200).send('working')
+  const subscriber = req.body
 
-  // fetch('https://actionnetwork.org/api/v2/people/', {
-  //       method: 'POST',
-  //       body: JSON.stringify(data),
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'OSDI-API-Token': process.env.AN_KEY
-  //       },
-  //   })
-  //   .then(res => {
-  //     if (!res.ok) {
-  //       throw "Request error"
-  //     }
-  //     return res.text()
-  //   })
-  //   .then(() => res.status(200).send('Successfully submitted to Action Network'))
-  //   .catch(err => res.status(500).send('Action Network request failed'));
+  const personData = {
+    email_addresses: [{
+      address: subscriber.email,
+      status: 'subscribed'
+    }],
+    family_name: subscriber.last_name,
+    given_name: subscriber.first_name,
+    postal_addresses: [{
+      postal_code: subscriber.zip
+    }],
+    country: "US",
+    language: "en",
+    customFields: []
+  };
+
+  const data = {
+    person: personData,
+  };
+
+  fetch('https://actionnetwork.org/api/v2/forms/70252a3c-235d-43b2-8761-aa9c559fb6fd/submissions/', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+          'OSDI-API-Token': process.env.AN_KEY
+        },
+    })
+    .then(res => {
+      if (!res.ok) {
+        throw "Request error"
+      }
+      return res.text()
+    })
+    .then(() => res.status(200).send('Successfully submitted to Action Network'))
+    .catch(err => res.status(500).send('Action Network request failed'));
 
 
 })
